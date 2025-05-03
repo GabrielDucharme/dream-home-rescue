85c2ddb4f7a14b7bf1635173325d87c50e384bfd
GabrielDucharme <gabriel.ducharme@gmail.com>
2025-05-03 14:54:27 -0400

    chore: Update package dependencies and improve type definitions
    - Updated various @payloadcms packages to version ^3.36.1 for better stability and features.
    - Upgraded next to version ^15.3.1 for enhanced performance and compatibility.
    - Added 'Australia/Brisbane' timezone to SupportedTimezones type.
    - Introduced optional 'placeholder' property in Form and FormsSelect interfaces for improved flexibility in form handling.

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -54,6 +54,7 @@ export type SupportedTimezones =
   | 'Asia/Singapore'
   | 'Asia/Tokyo'
   | 'Asia/Seoul'
+  | 'Australia/Brisbane'
   | 'Australia/Sydney'
   | 'Pacific/Guam'
   | 'Pacific/Noumea'
@@ -779,6 +780,7 @@ export interface Form {
             label?: string | null;
             width?: number | null;
             defaultValue?: string | null;
+            placeholder?: string | null;
             options?:
               | {
                   label: string;
@@ -2490,6 +2492,7 @@ export interface FormsSelect<T extends boolean = true> {
               label?: T;
               width?: T;
               defaultValue?: T;
+              placeholder?: T;
               options?:
                 | T
                 | {
